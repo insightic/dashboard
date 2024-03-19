1d202bbf59ddf9391303aad3aa8f615b03496e58
Yiwei Gong <imwithye@gmail.com>
2024-03-19 11:02:06 +0800

    add chart

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import VueApexCharts from 'vue3-apexcharts'
 import ElementPlus from 'element-plus'
 
 import './assets/main.scss'
@@ -10,6 +11,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueApexCharts)
 app.use(ElementPlus)
 
 app.mount('#app')
--- src/views/projectViews/FinancialAnalysisView.vue
+++ src/views/projectViews/FinancialAnalysisView.vue
@@ -1,1 +1,156 @@
-<template>Financial Analysis</template>
+<template>
+  <div class="mb-3">
+    <div class="h1 m-0">Financial Audit</div>
+    <div class="row">
+      <div class="col-md-12 my-2">
+        <CardComponent class="w-100">
+          <div class="d-flex">
+            Auditors: &nbsp;&nbsp;
+            <div v-html="render(data?.financial_analysis?.financial_audit?.auditors)"></div>
+          </div>
+          <div class="d-flex">
+            Score: &nbsp;&nbsp;
+            <div v-html="render(data?.financial_analysis?.financial_audit?.score)"></div>
+          </div>
+          <div class="d-flex">
+            Analysis: &nbsp;&nbsp;
+            <div v-html="render(data?.financial_analysis?.financial_audit?.analysis)"></div>
+
+            <div
+              class="mx-auto"
+              style="width: 540px"
+              v-if="
+                notEmpty(data?.financial_analysis?.financial_audit?.reserve_holdings_percentage)
+              "
+            >
+              <apexchart
+                width="540"
+                type="pie"
+                :options="{
+                  labels:
+                    data?.financial_analysis?.financial_audit?.reserve_holdings_percentage.map(
+                      (t) => t.title
+                    )
+                }"
+                :series="
+                  data?.financial_analysis?.financial_audit?.reserve_holdings_percentage.map(
+                    (t) => t.value
+                  )
+                "
+              />
+            </div>
+          </div>
+        </CardComponent>
+      </div>
+    </div>
+  </div>
+
+  <div class="mb-3">
+    <div class="h1 m-0">Market Capitalization and Liquidity</div>
+    <div class="row">
+      <div class="col-md-12 my-2">
+        <CardTable class="w-100 h-100">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(item, idx) in data?.financial_analysis?.market_capitalization_and_liquidity"
+              :key="idx"
+            >
+              <td
+                v-html="render(item.title)"
+                style="width: 30%; min-width: 120px; max-width: 300px; vertical-align: top"
+              ></td>
+              <td v-html="render(item.value)" style="vertical-align: top"></td>
+            </tr>
+          </tbody>
+        </CardTable>
+      </div>
+    </div>
+  </div>
+
+  <div class="mb-3">
+    <div
+      class="h1 m-0"
+      v-if="
+        data?.financial_analysis?.investments && data?.financial_analysis?.investments.length > 0
+      "
+    >
+      Investments
+    </div>
+    <div
+      class="row"
+      v-if="
+        data?.financial_analysis?.investments && data?.financial_analysis?.investments.length > 0
+      "
+    >
+      <div class="col-md-12 my-2">
+        <CardTable class="w-100 h-100">
+          <thead>
+            <tr>
+              <th>Round</th>
+              <th>Amount</th>
+              <th>Valuation</th>
+              <th>Date</th>
+              <th>Investors</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, idx) in data?.financial_analysis?.investments" :key="idx">
+              <td v-html="render(item.round)" style="vertical-align: top"></td>
+              <td v-html="render(item.amount)" style="vertical-align: top"></td>
+              <td v-html="render(item.valuation)" style="vertical-align: top"></td>
+              <td v-html="render(item.date)" style="vertical-align: top"></td>
+              <td v-html="render(item.investors)" style="vertical-align: top"></td>
+            </tr>
+          </tbody>
+        </CardTable>
+      </div>
+    </div>
+  </div>
+
+  <!-- <div class="mb-3">
+        <div class="h1 m-0" v-if="attr(data, 'financial_analysis', 'list_of_investors') &&
+                            attr(data, 'financial_analysis', 'list_of_investors').length > 0
+                            ">
+            List of Investors
+        </div>
+        <div class="row" v-if="attr(data, 'financial_analysis', 'list_of_investors') &&
+                            attr(data, 'financial_analysis', 'list_of_investors').length > 0
+                            ">
+            <div v-for="(item, idx) in attr(data, 'financial_analysis', 'list_of_investors')" :key="idx"
+                class="col-md-3 my-2">
+                <Card class="h-100">
+                    <div class="row align-items-center">
+                        <div class="col-auto">
+                            <span class="text-white avatar bg-white" style="box-shadow: none !important">
+                                <img :src="baseUrl + '/' + item.logo" class="w-100 h-100" />
+                            </span>
+                        </div>
+                        <div class="col">
+                            <div class="font-weight-medium">
+                                {{ item.name }}
+                            </div>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+        </div>
+    </div> -->
+</template>
+
+<script setup>
+import { render, notEmpty } from '@/helpers'
+
+import CardComponent from '@/components/CardComponent.vue'
+import CardTable from '@/components/CardTable.vue'
+
+defineProps({
+  data: Object,
+  baseUrl: String
+})
+</script>
