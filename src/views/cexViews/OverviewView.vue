53e81d61b43b619fe80a699e58682007d82e17ac
Yiwei Gong <imwithye@gmail.com>
2024-04-22 00:25:35 +0800

    add overview and history

--- src/views/cexViews/OverviewView.vue
+++ src/views/cexViews/OverviewView.vue
@@ -1,10 +1,20 @@
 <template>
-   <div class="mb-3">
+  <div class="mb-3">
     <div class="h1 m-0">Overview</div>
     <div class="row">
+      <div class="col-md-12 my-2">
+        <CardComponent class="h-100" subheader="Overview">
+          {{ data?.overview }}
+        </CardComponent>
+      </div>
+      <div class="col-md-12 my-2">
+        <CardComponent class="h-100" subheader="History">
+          {{ data?.history }}
+        </CardComponent>
+      </div>
       <div class="col-md-6 my-2">
         <CardComponent class="h-100" subheader="restricted countries">
-         <div v-for="(c, idx) in data?.restricted_countries" :key="idx">{{ c }}</div>
+          <div v-for="(c, idx) in data?.restricted_countries" :key="idx">{{ c }}</div>
         </CardComponent>
       </div>
       <div class="col-md-6 my-2">
@@ -19,14 +29,14 @@
       </div>
       <div class="col-md-6 my-2">
         <CardComponent class="h-100" subheader="margin trading">
-         <div>{{ data?.margin_trading?.availability }}</div>
-         <div>{{ data?.margin_trading?.leverage }}</div>
+          <div>{{ data?.margin_trading?.availability }}</div>
+          <div>{{ data?.margin_trading?.leverage }}</div>
         </CardComponent>
       </div>
       <div class="col-md-6 my-2">
         <CardComponent class="h-100" subheader="most active trading pair">
-         <div>{{ data?.most_active_trading_pair?.pair }}</div>
-         <div>{{ data?.most_active_trading_pair?.volume_24h }}</div>
+          <div>{{ data?.most_active_trading_pair?.pair }}</div>
+          <div>{{ data?.most_active_trading_pair?.volume_24h }}</div>
         </CardComponent>
       </div>
     </div>
@@ -34,7 +44,7 @@
 </template>
 
 <script lang="ts" setup>
-import CardComponent from '@/components/CardComponent.vue';
+import CardComponent from '@/components/CardComponent.vue'
 
 defineProps({
   data: Object
