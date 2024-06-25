ecf187bc26e0da23755e698ccdf219b9d6633170
Yiwei Gong <imwithye@gmail.com>
2024-06-25 22:03:50 +0800

    fix the distribution display

--- src/helpers.ts
+++ src/helpers.ts
@@ -44,7 +44,10 @@ export function render(str: string) {
 }
 
 export function formatNumber(num: number) {
-  if (num < 1000) return num.toFixed(1)
+  if (num < 1000) {
+    if (Number.isInteger(num)) return num
+    return num.toFixed(1)
+  }
   if (num < 1000000) return `${(num / 1000).toFixed(1)}K`
   if (num < 1000000000) return `${(num / 1000000).toFixed(1)}M`
   if (num < 1000000000000) return `${(num / 1000000000).toFixed(1)}B`
