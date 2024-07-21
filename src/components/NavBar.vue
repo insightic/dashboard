d705aa3609353e5fe96fe6aaf7f36c5a6c098300
Yiwei Gong <imwithye@gmail.com>
2024-07-21 20:49:36 +0800

    use element plus nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,38 +1,38 @@
 <template>
-  <nav class="fixed-top navbar navbar-expand navbar-dark bg-dark text-light" style="height: 56px">
-    <div class="container-xl">
-      <div
-        class="d-flex align-items-center"
-        style="width: 128px; cursor: pointer"
-        @click="router.push('/')"
-      >
-        <div class="me-2 rounded bg-light" style="padding: 4px">
-          <img src="/logo.png" alt="" width="24" height="24" />
-        </div>
-        <div class="m-0">Insightic</div>
-      </div>
-      <div class="m-0"></div>
-      <ul class="navbar-nav">
-        <li class="nav-item" style="cursor: pointer">
-          <a class="nav-link text-light" @click="goto('stablecoins')">Stablecoins</a>
-        </li>
-        <li class="nav-item" style="cursor: pointer">
-          <a class="nav-link text-light" @click="goto('cex')">CEX</a>
-        </li>
-        <li class="nav-item" style="cursor: pointer">
-          <a class="nav-link text-light" @click="goto('lending')">Lending</a>
-        </li>
-      </ul>
+  <el-menu
+    mode="horizontal"
+    :default-active="activeIndex"
+    :ellipsis="false"
+    class="fixed-top"
+    style="position: fixed !important"
+  >
+    <div class="d-flex justify-content-center align-items-center px-2">
+      <img class="me-2" src="/logo.png" alt="" width="24" height="24" />
+      <b>Insightic</b>
     </div>
-  </nav>
+    <div class="flex-grow-1"></div>
+    <el-menu-item index="1" @click="goto(1, 'stablecoins')">
+      <div>Stablecoins</div>
+    </el-menu-item>
+    <el-menu-item index="2" @click="goto(2, 'cex')">
+      <div>CEX</div>
+    </el-menu-item>
+    <el-menu-item index="3" @click="goto(3, 'lending')">
+      <div>Leading</div>
+    </el-menu-item>
+  </el-menu>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 const router = useRouter()
 const emits = defineEmits(['change'])
 
-function goto(type: string) {
+const activeIndex = ref('1')
+
+function goto(idx: number, type: string) {
+  activeIndex.value = idx.toString()
   router.push(`/${type}`)
   emits('change', type)
 }
