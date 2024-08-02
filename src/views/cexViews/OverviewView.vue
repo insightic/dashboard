dee5d28cdc37451ee14e9ed6a0113fa116efe2c0
Yiwei Gong <imwithye@gmail.com>
2024-08-02 17:29:11 +0800

    fix overview

--- src/views/cexViews/OverviewView.vue
+++ src/views/cexViews/OverviewView.vue
@@ -13,34 +13,16 @@
         </CardComponent>
       </div>
       <div class="col-md-12 my-2">
-        <CardTable style="max-height: 300px">
-          <thead class="sticky-top">
-            <tr>
-              <th>Rank</th>
-              <th>Currency</th>
-              <th>Pair</th>
-              <th>Price</th>
-              <th>+2% Depth</th>
-              <th>-2% Depth</th>
-              <th>Volume</th>
-              <th>Volume %</th>
-              <th>Updated</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(pair, idx) in data?.coinMarketCap?.pair_table_data" :key="idx">
-              <td>{{ pair.Rank }}</td>
-              <td>{{ pair.Currency }}</td>
-              <td>{{ pair.Pair }}</td>
-              <td>{{ pair.Price }}</td>
-              <td>{{ pair['+2% Depth'] }}</td>
-              <td>{{ pair['-2% Depth'] }}</td>
-              <td>{{ pair.Volume }}</td>
-              <td>{{ pair['Volume %'] }}</td>
-              <td>{{ data?.coinMarketCap?.created_date }}</td>
-            </tr>
-          </tbody>
-        </CardTable>
+        <el-table :border="true" :data="data?.coinMarketCap?.pair_table_data">
+          <el-table-column label="Rank" prop="Rank" />
+          <el-table-column label="Currency" prop="Currency" />
+          <el-table-column label="Pair" prop="Pair" />
+          <el-table-column label="Price" prop="Price" />
+          <el-table-column label="+2% Depth" prop="+2% Depth" />
+          <el-table-column label="-2% Depth" prop="-2% Depth" />
+          <el-table-column label="Volume" prop="Volume" />
+          <el-table-column label="Volume %" prop="Volume %" />
+        </el-table>
       </div>
       <div class="col-md-6 my-2">
         <CardComponent class="h-100" :subheader="`${data?.name} restricted countries`">
